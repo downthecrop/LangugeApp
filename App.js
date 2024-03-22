e3f75c9797f77418157cd1e37739148fb22c4b6e
downthecrop <bryce@mathx.ca>
2024-03-22 12:56:05 +0900

    .

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import Animated, { Layout, FadeOut, FadeIn } from 'react-native-reanimated';
 
-export default function App() {
+const OptionItem = ({ item, onRemove, onAdd }) => {
+  const handlePress = () => {
+    if (onRemove) {
+      onRemove(item);
+    } else if (onAdd) {
+      onAdd(item);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.View
+      entering={FadeIn}
+      exiting={FadeOut}
+      layout={Layout.springify().damping(50)}
+      style={styles.optionItem}
+    >
+      <TouchableOpacity onPress={handlePress}>
+        <Text style={styles.optionText}>{item}</Text>
+      </TouchableOpacity>
+    </Animated.View>
   );
-}
+};
+
+const App = () => {
+  const [options, setOptions] = useState(['받침', '더', '제시해', '주세요', '드세요', '잠']);
+  const [answers, setAnswers] = useState([]);
+
+  const removeOption = (optionToRemove) => {
+    setOptions(options.filter(option => option !== optionToRemove));
+    setAnswers([...answers, optionToRemove]);
+  };
+
+  const addOption = (optionToAdd) => {
+    setAnswers(answers.filter(answer => answer !== optionToAdd));
+    setOptions([...options, optionToAdd]);
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={[styles.title, { marginTop: 20 }]}>Answers</Text>
+      <View style={[styles.optionsContainer, {    borderBottomColor: 'white',
+    borderBottomWidth: 1}]}>
+        {answers.map((answer) => (
+          <OptionItem key={answer} item={answer} onAdd={addOption} />
+        ))}
+      </View>
+      <View style={styles.optionsContainer}>
+        {options.map((option) => (
+          <OptionItem key={option} item={option} onRemove={removeOption} />
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexGrow: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    backgroundColor: '#041121',
+  },
+  title: {
+    color: '#fdfffc',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  optionsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
+    minHeight: 50,
+  },
+  optionItem: {
+    backgroundColor: '#2c3848',
+    padding: 10,
+    borderRadius: 4,
+    margin: 5,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  optionText: {
+    color: '#f9feff',
+    fontSize: 18,
+  },
 });
+
+export default App;
